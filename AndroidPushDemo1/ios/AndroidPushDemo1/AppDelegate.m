20e4ab6c56088df9ccfa121fb389385f1a39a8cb
Tuo04 <chentuo01@beyondsoft.com>
2017-06-21 05:14:33 -0700

    Add new file

--- AndroidPushDemo1/ios/AndroidPushDemo1/AppDelegate.m
+++ AndroidPushDemo1/ios/AndroidPushDemo1/AppDelegate.m
@@ -8,6 +8,10 @@
  */
 
 #import "AppDelegate.h"
+#import <RNCrashes/RNCrashes.h>
+
+#import <RNAnalytics/RNAnalytics.h>
+
 #import <RNPush/RNPush.h>
 
 
@@ -19,6 +23,10 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
+
+  [RNCrashes registerWithCrashDelegate: [[RNCrashesDelegateAlwaysSend alloc] init]];  // Initialize Mobile Center crashes
+
+  [RNAnalytics registerWithInitiallyEnabled:true];  // Initialize Mobile Center analytics
 
   [RNPush registerAndEnable];  // Initialize Mobile Center push
 
